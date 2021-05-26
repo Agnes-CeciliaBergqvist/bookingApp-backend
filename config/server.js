a4fff546ebfdd29ecbf4d05321dc1bef683502e7
Agnes-CeciliaBergqvist <agnes.bergqvist@hotmail.com>
2021-05-26 11:20:53 +0200

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '47eec1f53380b15d065ceba0136425cb'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'dcdfdfb6-799f-4499-9f75-e22b04e25636'
+};
